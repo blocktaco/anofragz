e826cf2fc4a7d6c9034f642813c8e07eab38616d
blocktaco <60831236+blocktaco@users.noreply.github.com>
2021-02-20 20:33:10 -0600

    Update anocope.lua

--- anocope.lua
+++ anocope.lua
@@ -81,12 +81,12 @@ function newLibrary()
 
         local folder = util:Create('Frame', {Name = "Folder", Parent = gui, AnchorPoint = Vector2.new(0.5, 0.5), BackgroundColor3 = Color3.fromRGB(255, 255, 255), BorderColor3 = Color3.fromRGB(0, 0, 0), Position = UDim2.new(0.515999973, 0, 0.291036427, 0), Size = UDim2.new(0, 196, 0, 21), ZIndex = 1})
         local titleHolder = util:Create('Frame', {Name = "TitleHolder", Parent = folder, AnchorPoint = Vector2.new(0.5, 0.5), BackgroundColor3 = Color3.fromRGB(36, 36, 36), BorderColor3 = Color3.fromRGB(0, 0, 0), BorderSizePixel = 0, Position = UDim2.new(0.5, 0, 0.5, 0), Size = UDim2.new(0, 196, 0, 21), ZIndex = 1})
-        local title = util:Create('TextLabel', {
-            Name = "Title", Parent = titleHolder, BackgroundColor3 = Color3.fromRGB(255, 255, 255), BackgroundTransparency = 1.000, BorderColor3 = Color3.fromRGB(53, 53, 53), BorderSizePixel = 0, Size = UDim2.new(0, 195, 0, 21), Font = Enum.Font.GothamBold, Text = folderName, TextColor3 = Color3.fromRGB(255, 255, 255), TextSize = 14.000, TextStrokeTransparency = 0.500, TextWrapped = true,
-            util:Create('UIGradient', {
-                Color = ColorSequence.new{ColorSequenceKeypoint.new(0, Color3.fromRGB(200, 200, 200)), ColorSequenceKeypoint.new(0.5, Color3.fromRGB(255, 255, 255)), ColorSequenceKeypoint.new(1, Color3.fromRGB(200, 200, 200))}, Rotation = 90
-            }),
-        })
+        local title = util:Create('TextLabel', {Name = "Title", Parent = titleHolder, BackgroundColor3 = Color3.fromRGB(255, 255, 255), BackgroundTransparency = 1.000, BorderColor3 = Color3.fromRGB(53, 53, 53), BorderSizePixel = 0, Size = UDim2.new(0, 195, 0, 21), Font = Enum.Font.GothamBold, Text = folderName, TextColor3 = Color3.fromRGB(255, 255, 255), TextSize = 14.000, TextStrokeTransparency = 0.500, TextWrapped = true})
+        local uiGradient = Instance.new('UIGradient')
+        uiGradient.Parent = titleHolder
+        uiGradient.Color = ColorSequence.new{ColorSequenceKeypoint.new(0.00, Color3.fromRGB(200, 200, 200)), ColorSequenceKeypoint.new(0.51, Color3.fromRGB(255, 255, 255)), ColorSequenceKeypoint.new(1.00, Color3.fromRGB(200, 200, 200))}
+        uiGradient.Rotation = -90
+
         local background = util:Create('Frame', {Name = "Background", Parent = titleHolder, AnchorPoint = Vector2.new(0.5, 0.5), BackgroundColor3 = Color3.fromRGB(0, 0, 0), BackgroundTransparency = 1.000, BorderColor3 = Color3.fromRGB(0, 0, 0), Position = UDim2.new(0.5, 0, 8.34000015, 0), Selectable = true, Size = UDim2.new(0, 194, 0, 306)})
         local holder = util:Create('Frame', {
             Name = "Holder", Parent = background, AnchorPoint = Vector2.new(0.5, 0.5), BackgroundColor3 = Color3.fromRGB(49, 49, 49), BorderColor3 = Color3.fromRGB(0, 0, 0), Position = UDim2.new(0.5, 0, 0.5, 0), Size = UDim2.new(0, 194, 0, 306),
